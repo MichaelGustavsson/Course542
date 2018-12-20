d288358931ea7d8888ed0d40751b8d8a57c113d4
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2018-12-20 13:28:46 +0100

    Initial Commit

---
+++ XSLTDemo/XSLTDemo/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XSLTDemo.Startup))]
+namespace XSLTDemo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
